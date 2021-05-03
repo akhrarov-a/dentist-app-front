790bede1e07b0dbf6a8a231b427aa7d1301e45ab
akhrarov-a <asad.lion06@gmail.com>
2021-05-04 00:34:21 +0500

    #configured patients page to display patients from reducer #added search component and corresponding handlers

--- src/pages/Patients/index.tsx
+++ src/pages/Patients/index.tsx
@@ -1,3 +1,6 @@
+import { Body, Cell, Head, Pagination, Row, Table } from '../../core';
+import { BsInfoCircle } from 'react-icons/all';
+import PatientsNavbar from '../../components/PatientsNavbar';
 import React from 'react';
 import styles from './style.module.scss';
 import usePatientsPage from './props';
@@ -6,11 +9,46 @@ import usePatientsPage from './props';
  * Patients Page
  */
 const PatientsPage: React.FC = (): JSX.Element => {
-  const {} = usePatientsPage();
+  const {
+    patients,
+    query,
+    total,
+    onPageChange,
+    onQueryChange
+  } = usePatientsPage();
 
   return (
-    <div>
-      <p>Patients Page</p>
+    <div className={styles.container}>
+      <PatientsNavbar query={query} onQueryChange={onQueryChange} />
+      <div className={styles['patients-container']}>
+        <Table>
+          <Head>
+            <Row className={styles.row}>
+              <Cell className={styles.pl}>Name</Cell>
+              <Cell>Email</Cell>
+              <Cell>Phone number</Cell>
+              <Cell>Description</Cell>
+              <Cell />
+            </Row>
+          </Head>
+          <Body>
+            {patients?.map(
+              ({ name, phoneNumber, email, description }, index) => (
+                <Row key={index} className={styles.row}>
+                  <Cell className={styles.pl}>{name}</Cell>
+                  <Cell>{email}</Cell>
+                  <Cell>{phoneNumber}</Cell>
+                  <Cell>{description?.substr(0, 14)}</Cell>
+                  <Cell>
+                    <BsInfoCircle className={styles.iconInfo} />
+                  </Cell>
+                </Row>
+              )
+            )}
+          </Body>
+        </Table>
+      </div>
+      {!query && <Pagination length={total} onPageChange={onPageChange} />}
     </div>
   );
 };
--- src/pages/Patients/props.ts
+++ src/pages/Patients/props.ts
@@ -1,6 +1,32 @@
+import { AppState } from '../../api/models/app-state';
+import { getPatients } from '../../redux/modules/patients/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
 /**
  * Patients Page Props
  */
-const usePatientsPage = () => ({ message: 'Asadbek' });
+const usePatientsPage = () => {
+  const dispatch = useDispatch();
+
+  const { patients, total } = useSelector((state: AppState) => state.patients);
+
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [query, setQuery] = useState('');
+
+  const onQueryChange = (value: string) => {
+    setQuery(value);
+  };
+
+  const onPageChange = ({ selected }: { selected: number }) => {
+    setSelectedPage(selected + 1);
+  };
+
+  useEffect(() => {
+    dispatch(getPatients(selectedPage, query));
+  }, [selectedPage, query]);
+
+  return { patients, query, total, onPageChange, onQueryChange };
+};
 
 export default usePatientsPage;
