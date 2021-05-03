c30e2255bc7ba0057f6b22ce71017025ec2d523d
akhrarov-a <asad.lion06@gmail.com>
2021-05-04 00:40:58 +0500

    #added patients state to app state type

--- src/api/models/app-state/index.ts
+++ src/api/models/app-state/index.ts
@@ -1,8 +1,10 @@
 import AuthReducerState from '../reducer-states/auth.state';
+import PatientsReducerState from '../reducer-states/patients.state';
 
 /**
  * App State Type
  */
 export type AppState = {
   auth: AuthReducerState;
+  patients: PatientsReducerState;
 };
