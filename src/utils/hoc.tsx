11e9fa69d29bb98abe188ea1021c349ae6107560
akhrarov-a <asad.lion06@gmail.com>
2021-05-05 23:53:24 +0500

    #hoc util added

---
+++ src/utils/hoc.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { ComponentType } from 'react';
+
+const hoc = function <SP, HP>(
+  hook: (props: Partial<SP>) => HP,
+  Source: ComponentType<HP & SP>
+) {
+  const Result: any = (props: SP) => (
+    <Source {...(hook(props) || ({} as any))} {...props} />
+  );
+
+  Result.Original = Source;
+  Result.hook = hook;
+
+  return (Result as any) as ComponentType<Partial<HP> & Partial<SP>> & {
+    Original: ComponentType<HP & SP>;
+    hook: typeof hook;
+  };
+};
+
+export { hoc };
