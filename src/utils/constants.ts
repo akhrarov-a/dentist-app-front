d1b5604d1539246a0ac8bdebe85ea6dd6df8e7da
akhrarov-a <asad.lion06@gmail.com>
2021-05-23 22:30:28 +0500

    #url config for development

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,6 +10,6 @@ export const apiBaseUrl = 'https://stomatolog-uz-api.herokuapp.com/api/v1';
  * Front base url
  */
 // Development
-// export const frontBaseUrl = 'http://localhost:3000';
+export const frontBaseUrl = 'http://localhost:3000';
 // Production
-export const frontBaseUrl = 'https://stomatolog-uz.herokuapp.com';
+// export const frontBaseUrl = 'https://stomatolog-uz.herokuapp.com';
